91cd527e3b84c69a35f3bd2901709e77e02d77a0
vinsburg <vinsburg@gmail.com>
2022-09-24 15:51:48 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// Data source: [Conversion of scales of temperature](https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature)
+// struct Unit {
+//     ratio: f64,
+//     del_x: f64,
+//     del_y: f64,
+// }
+
+// const fahr: Unit {}
+
+// fn dest_to_orig(temp_fahr: f64) {
+//     // TODO
+// }
+
+// fn orig_to_dest(temp_fahr: f64) {
+//     // TODO
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
