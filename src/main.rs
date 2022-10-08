b12842b79fe00ac1dfcc748a1d643790c8da877a
vinsburg <vinsburg@gmail.com>
2022-10-08 18:14:54 +0300

    Removed redundant scale data

--- src/main.rs
+++ src/main.rs
@@ -8,44 +8,35 @@ enum Scale {
 }
 
 #[derive(Debug)]
-struct ReferenceScaleData {
-    ratio: f64,
-    del_x: f64,
-    del_y: f64,
-    ref_scale: Scale,
+struct SlopeIntercpet {
+    slope: f64,
+    y_intercept: f64,
 }
 
 impl Scale {
-    const fn get_ref_scale_data(&self) -> ReferenceScaleData {
-        let ref_scale = Scale::Kelvin;
+    const REF_SCALE: Scale = Scale::Kelvin;
+    const fn get_ref_slope_intercept(&self) -> SlopeIntercpet {
         match *self {
-            Scale::Kelvin => ReferenceScaleData {
-                ratio: 1.0,
-                del_x: 0.0,
-                del_y: 0.0,
-                ref_scale: ref_scale,
+            Scale::Kelvin => SlopeIntercpet {
+                slope: 1.0,
+                y_intercept: 0.0,
             },
-            Scale::Celsius => ReferenceScaleData {
-                ratio: 1.0,
-                del_x: 273.15,
-                del_y: 0.0,
-                ref_scale: ref_scale,
+            Scale::Celsius => SlopeIntercpet {
+                slope: 1.0,
+                y_intercept: 273.15,
             },
-            Scale::Fahrenheit => ReferenceScaleData {
-                ratio: 0.55555555555, // 5.0/9.0
-                del_x: 459.67,
-                del_y: 0.0,
-                ref_scale: ref_scale,
+            Scale::Fahrenheit => SlopeIntercpet {
+                slope: 0.5555555555555556, // 5.0/9.0
+                y_intercept: 255.37222222222223,
             },
-            Scale::Romer => ReferenceScaleData {
-                ratio: 1.90476190476, // 40.0/21.0
-                del_x: -7.5,
-                del_y: 273.15,
-                ref_scale: ref_scale,
+            Scale::Romer => SlopeIntercpet {
+                slope: 1.9047619047619047, // 40.0/21.0
+                y_intercept: 258.8642857142857,
             },
         }
     }
 }
+
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -56,41 +47,42 @@ struct Temperature {
 
 impl Temperature {
     fn convert_to_ref_scale(&self) -> Temperature {
-        let ref_scale_data = self.scale.get_ref_scale_data();
+        let ref_scale_data = self.scale.get_ref_slope_intercept();
         Temperature {
-            value: (self.value + ref_scale_data.del_x) * ref_scale_data.ratio + ref_scale_data.del_y,
-            scale: ref_scale_data.ref_scale,
+            value: self.value * ref_scale_data.slope + ref_scale_data.y_intercept,
+            scale: Scale::REF_SCALE,
         }
     }
     
     fn convert_to(&self, new_scale: Scale) -> Temperature {
-        let ref_scale_data = new_scale.get_ref_scale_data();
+        let ref_scale_data = new_scale.get_ref_slope_intercept();
         let ref_temp = self.convert_to_ref_scale();
         Temperature {
-            value: (ref_temp.value - ref_scale_data.del_y) / ref_scale_data.ratio - ref_scale_data.del_x,
+            value: ( ref_temp.value - ref_scale_data.y_intercept ) / ref_scale_data.slope,
             scale: new_scale,
         }
     }
 }
 
-// fn display_conversion(scale_a: fn() -> ReferenceScaleData, scale_b: fn() -> ReferenceScaleData, val_a: f64) {
+// fn display_conversion(scale_a: fn() -> SlopeIntercpet, scale_b: fn() -> SlopeIntercpet, val_a: f64) {
 //     let val_b = convert(scale_a(), scale_b(), val_a);
 //     println!("{val_a:.2} {:10} is {val_b:.2} {:10}", scale_a().name, scale_b().name);
 // }
 
 fn main() {
     println!("\nWelcome to the temperature converter demo!\n");
-    let demo_scale = Scale::Kelvin;
-    let demo_ref_scale_data = demo_scale.get_ref_scale_data();
-    println!("{:?} {:?}", demo_scale, demo_ref_scale_data);
     
     
     let temp = Temperature { value: 25.0, scale: Scale::Celsius };
-    println!("\nCurrent {:?}", temp);
-    println!("{:?} {:?}", temp.scale, temp.scale.get_ref_scale_data());
-    println!("Reference {:?}\n", temp.convert_to_ref_scale());
+    println!("Current {:?}\n", temp);
+    println!("The reference scale is {:?}\n", Scale::REF_SCALE);
+    println!("{:?} to {:?} {:?}\n", temp.scale, Scale::REF_SCALE, temp.scale.get_ref_slope_intercept());
+    let demo_scale = Scale::Kelvin;
+    println!("{:?} to {:?} {:?}\n", demo_scale, Scale::REF_SCALE, demo_scale.get_ref_slope_intercept());
     
-    println!("Result of conversion to {:?} is: {:?}", Scale::Celsius, temp.convert_to(Scale::Celsius));
-    println!("Result of conversion to {:?} is: {:?}", Scale::Fahrenheit, temp.convert_to(Scale::Fahrenheit));
-    println!("Result of conversion to {:?} is: {:?}", Scale::Romer, temp.convert_to(Scale::Romer));
+    println!("Let's convert the current temperature to various scales:");
+    println!("{:>10.2} {:?} ", temp.convert_to_ref_scale().value, Scale::REF_SCALE, );
+    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Celsius).value, Scale::Celsius, );
+    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Fahrenheit).value, Scale::Fahrenheit, );
+    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Romer).value, Scale::Romer, );
 }
