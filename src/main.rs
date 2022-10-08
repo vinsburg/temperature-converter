db89b3b00c8f9be026e090bb4e1865f53f462c60
vinsburg <vinsburg@gmail.com>
2022-10-08 22:27:15 +0300

    Scale derives copy - fixed SlopeIntercept typo - print various temperatures with for loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Data source: [Conversion of scales of temperature](https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature)
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 enum Scale {
     Kelvin,
     Celsius,
@@ -8,28 +8,28 @@ enum Scale {
 }
 
 #[derive(Debug)]
-struct SlopeIntercpet {
+struct SlopeIntercept {
     slope: f64,
     y_intercept: f64,
 }
 
 impl Scale {
     const REF_SCALE: Scale = Scale::Kelvin;
-    const fn get_slope_intercept(&self) -> SlopeIntercpet {
+    const fn get_slope_intercept(&self) -> SlopeIntercept {
         match *self {
-            Scale::Kelvin => SlopeIntercpet {
+            Scale::Kelvin => SlopeIntercept {
                 slope: 1.0,
                 y_intercept: 0.0,
             },
-            Scale::Celsius => SlopeIntercpet {
+            Scale::Celsius => SlopeIntercept {
                 slope: 1.0,
                 y_intercept: 273.15,
             },
-            Scale::Fahrenheit => SlopeIntercpet {
+            Scale::Fahrenheit => SlopeIntercept {
                 slope: 0.5555555555555556, // 5.0/9.0
                 y_intercept: 255.37222222222223, // 459.67*5.0/9.0
             },
-            Scale::Romer => SlopeIntercpet {
+            Scale::Romer => SlopeIntercept {
                 slope: 1.9047619047619047, // 40.0/21.0
                 y_intercept: 258.8642857142857, // 273.15-7.5*40.0/21.0
             },
@@ -64,24 +64,19 @@ impl Temperature {
     }
 }
 
-// fn display_conversion(scale_a: fn() -> SlopeIntercpet, scale_b: fn() -> SlopeIntercpet, val_a: f64) {
-//     let val_b = convert(scale_a(), scale_b(), val_a);
-//     println!("{val_a:.2} {:10} is {val_b:.2} {:10}", scale_a().name, scale_b().name);
-// }
-
 fn main() {
     println!("\nWelcome to the temperature converter demo!\n");
     
     let temp = Temperature { value: 25.0, scale: Scale::Celsius };
     println!("Current {:?}\n", temp);
     println!("The reference scale is {:?}\n", Scale::REF_SCALE);
-    println!("{:?} to {:?} {:?}\n", temp.scale, Scale::REF_SCALE, temp.scale.get_slope_intercept());
+    println!("{:?} to {:?} {:?}", temp.scale, Scale::REF_SCALE, temp.scale.get_slope_intercept());
+    println!("Current temperature in {:?} scale is {:?}°\n", Scale::REF_SCALE, temp.convert_to_ref_scale().value);
     let demo_scale = Scale::Kelvin;
-    println!("{:?} to {:?} {:?}\n", demo_scale, Scale::REF_SCALE, demo_scale.get_slope_intercept());
+    println!("{:?} to {:?} {:?}", demo_scale, Scale::REF_SCALE, demo_scale.get_slope_intercept());
     
-    println!("Let's convert the current temperature to various scales:");
-    println!("{:>10.2} {:?} ", temp.convert_to_ref_scale().value, Scale::REF_SCALE, );
-    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Celsius).value, Scale::Celsius, );
-    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Fahrenheit).value, Scale::Fahrenheit, );
-    println!("{:>10.2} {:?} ", temp.convert_to(Scale::Romer).value, Scale::Romer, );
+    println!("\nLet's convert the current temperature to various scales:");
+    for scale in [Scale::Kelvin, Scale::Celsius, Scale::Fahrenheit, Scale::Romer] {
+        println!("T = {:>6.2}° {:?} ", temp.convert_to(scale).value, scale, );
+    }
 }
